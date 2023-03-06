fa89f6f027ab1bf9c35f00cfcd8edd4790410612
Ashley Hawkins <536669@escg.ac.uk>
2023-03-06 16:14:47 +0000

    Player death and run animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     HealthAndCombat healthAndCombat;
     Animator anim;
+    SpriteRenderer sr;
 
     public float normalSpeedMultiplier = 1;
     public float powerupSpeedMultiplier = 1.5f;
@@ -25,11 +26,13 @@ public class Player : MonoBehaviour
         Gizmos.matrix = transform.localToWorldMatrix;
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(attackBoundingBox.center, attackBoundingBox.size);
+        Gizmos.DrawWireCube(new Vector2(-attackBoundingBox.center.x, attackBoundingBox.center.y), attackBoundingBox.size);
     }
 
 
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         healthAndCombat = GetComponent<HealthAndCombat>();
         healthAndCombat.OnDamage += OnDamage;
@@ -47,7 +50,7 @@ public class Player : MonoBehaviour
 
     void OnDeath()
     {
-
+        anim.Play("PlayerDeath");
     }
 
     public IEnumerator ApplySpeedPowerup()
@@ -69,26 +72,56 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        if (!healthAndCombat.alive) return;
         if (Input.GetKeyDown(KeyCode.Space))
         {
             wantsJump = true;
         }
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && !anim.GetCurrentAnimatorStateInfo(0).IsName("PlayerAttack"))
         {
-            healthAndCombat.TryAttack(attackBoundingBox, (int)(10 * currentDamageMultiplier));
+            anim.Play("Attack");
+            int damage = (int)(10 * currentDamageMultiplier);
+            Rect attackRect = attackBoundingBox;
+
+            if (sr.flipX)
+            {
+                attackRect.center = new Vector2(-attackRect.center.x, attackRect.center.y);
+            }
+            healthAndCombat.TryAttack(attackRect, damage);
+
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            healthAndCombat.DealDamage(1000000, Vector2.zero);
         }
     }
 
     void FixedUpdate()
     {
+        if (!healthAndCombat.alive) return;
         float verticalVel = 0;
         if (Input.GetKey(KeyCode.A))
         {
             verticalVel -= 1;
+            sr.flipX = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
             verticalVel += 1;
+            sr.flipX = false;
+        }
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Attack") || anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f)
+        {
+            if (Mathf.Abs(verticalVel) > 0.1)
+            {
+                if (!anim.GetCurrentAnimatorStateInfo(0).IsName("PlayerRun"))
+                    anim.Play("PlayerRun");
+            }
+            else
+            {
+                if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+                    anim.Play("Idle");
+            }
         }
         if (wantsJump)
         {
