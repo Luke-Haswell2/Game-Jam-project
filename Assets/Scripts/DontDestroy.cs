4bf35266769570b38086bc60e978f2942e85807b
Abhijeet Goush <530082@escg.ac.uk>
2023-03-10 12:08:42 +0000

    Music

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    public static DontDestroy instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
